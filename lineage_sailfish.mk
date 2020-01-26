d3623b885b317dd4928a0bcea06b49862f84cf97
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-01-26 03:48:49 +0000

    Remove aosp_marlin.mk and aosp_sailfish.mk
    Those .mk were merged with lineage_marlin.mk and
    lineage_sailfish.mk, there is no need to keep them as they
    can be problematic when building AOSP ROM's such as PE, AEX, etc...

--- lineage_sailfish.mk
+++ lineage_sailfish.mk
@@ -1,3 +1,26 @@
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
@@ -15,10 +38,14 @@ $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_sailfish
+PRODUCT_DEVICE := sailfish
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel
 TARGET_MANUFACTURER := HTC
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Fstab common
+PRODUCT_COPY_FILES += device/google/marlin/fstab.common:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.sailfish
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
@@ -26,4 +53,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := google/sailfish/sailfish:10/QP1A.191005.007.A3/5972272:user/release-keys
 
+$(call inherit-product, device/google/marlin/device-sailfish.mk)
+$(call inherit-product-if-exists, vendor/google_devices/marlin/device-vendor-sailfish.mk)
 $(call inherit-product-if-exists, vendor/google/sailfish/sailfish-vendor.mk)
+
+PRODUCT_PACKAGES += \
+    Launcher3QuickStep \
+    WallpaperPicker
